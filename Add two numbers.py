846b65c309399ffcf8b51c42a8484d01b38870c4
SABARIRAJKUMAR <87665718+SABARIRAJKUMAR@users.noreply.github.com>
2021-09-19 18:36:16 +0530

    Update Add two numbers.py

--- Add two numbers.py
+++ Add two numbers.py
@@ -1,4 +1,4 @@
 num1=input("enter the num1:")
 num2=input("enter th num2:")
 sum=int(num1)+int(num2)
-print("value of {0} and {1} is {2}",num1,num2,sum)
+print("value of num1 and num2 is sum",num1,num2,sum)
